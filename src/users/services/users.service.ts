042852829fb170e01bf1b5e8ee4535fd15947616
Sergey Serzhan <serzhansergey96@gmail.com>
2022-07-18 16:41:37 +0300

    feat: create getUser, createUser, updateUser, getAutoSuggestUsers, deleteUser controllers methods

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,31 +1,53 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 } from 'uuid';
 
-import { User } from "../interfaces/user.interface";
-import { CreateUserDto } from "../dto/create-user-dto";
-import { UpdateUserDto } from "../dto/update-user-dto";
+import { User } from '../interfaces/user.interface';
+import { CreateUserDto } from '../dto/create-user-dto';
+import { UpdateUserDto } from '../dto/update-user-dto';
+
+import { getSortFuncObjByStringKey } from '../utils/sort';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
+  private users: User[] = [];
 
-  async findOne(id: string): Promise<User> {
-    const user = this.users.find(user => user.id === id);
+  async getUser(id: string): Promise<User> {
+    const user = this.users
+      .filter((user) => !user.isDeleted)
+      .find((user) => user.id === id);
     if (!user) throw new NotFoundException('User with this id not found');
     return user;
   }
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = {...createUserDto, id: v4(), isDeleted: false};
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = { ...createUserDto, id: v4(), isDeleted: false };
     this.users.push(newUser);
     return newUser;
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    const user = await this.findOne(id);
-    const userIndex = this.users.findIndex(user => user.id === id);
-    const updatedUser = {...user, ...updateUserDto};
-    this.users[userIndex] = updatedUser;
-    return updatedUser;
+  async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.getUser(id);
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    const newUser = { ...user, ...updateUserDto };
+    this.users[userIndex] = newUser;
+    return newUser;
+  }
+
+  async getAutoSuggestUsers(
+    loginSubstring: string,
+    limit: number,
+  ): Promise<User[]> {
+    return this.users
+      .filter(
+        (user) =>
+          !user.isDeleted &&
+          user.login.toLowerCase().includes(loginSubstring.toLowerCase()),
+      )
+      .sort(getSortFuncObjByStringKey('login'))
+      .slice(0, limit);
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    (await this.getUser(id)).isDeleted = true;
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+import { UsersService } from './services/users.service';
+import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
+
+@Controller('v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<User> {
+    return await this.usersService.getUser(id);
+  }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return await this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Get()
+  async getAutoSuggestUsers(
+    @Query('search') loginSubstring: string,
+    @Query('limit') limit: number,
+  ): Promise<User[]> {
+    return await this.usersService.getAutoSuggestUsers(loginSubstring, limit);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async deleteUser(@Param('id') id: string): Promise<void> {
+    return this.usersService.deleteUser(id);
+  }
+}
