8e3da6f064b1ebcd7f2582c07e967f689be4126a
SergeySerzhan <s.sergey96@mail.ru>
2022-08-03 09:07:17 +0300

    fix: create data-access directory and users.repository

--- src/users/utils/check-data.ts
+++ src/users/utils/check-data.ts
@@ -1,5 +1,5 @@
 import { User } from '../models/user.model';
 
-export function checkUser(user: User): void {
+export function checkData(user: User | number): void {
   if (!user) throw new Error('No user');
 }
