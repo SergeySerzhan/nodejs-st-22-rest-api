7757f75a35ba733f828ce1c5d61e14d227923bde
SergeySerzhan <s.sergey96@mail.ru>
2022-08-08 15:35:23 +0300

    feat: add global logger middleware

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './filters/all-exception.filter';
+import { globalLogger } from './shared/middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+
+  app.use(globalLogger);
 
   await app.listen(process.env.PORT || 3000);
 }
